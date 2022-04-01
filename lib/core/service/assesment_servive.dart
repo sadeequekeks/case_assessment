cdc05b3dbe0f6a5010a2e4a4154cb3c4cb3a6026
sadeequekeks <iazubair.ug@atbu.edu.ng>
2022-04-01 13:00:21 +0100

    done

--- lib/core/service/assesment_servive.dart
+++ lib/core/service/assesment_servive.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 import '../service_injector/service_injector.dart';
 
@@ -57,4 +59,41 @@ class AssessmentService {
     }
     return user;
   }
+  // static Future<User?> signInWithGoogle({required BuildContext context}) async {
+  //   FirebaseAuth auth = FirebaseAuth.instance;
+  //   User? user;
+
+  //   // final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  //   // final GoogleSignInAccount? googleSignInAccount =
+  //   //     await googleSignIn.signIn();
+
+  //   // if (googleSignInAccount != null) {
+  //   //   final GoogleSignInAuthentication googleSignInAuthentication =
+  //   //       await googleSignInAccount.authentication;
+
+  //     final AuthCredential credential = GoogleAuthProvider.credential(
+  //       // accessToken: googleSignInAuthentication.accessToken,
+  //       // idToken: googleSignInAuthentication.idToken,
+  //     );
+
+  //     try {
+  //       final UserCredential userCredential =
+  //           await auth.signInWithCredential(credential);
+
+  //       user = userCredential.user;
+  //     } on FirebaseAuthException catch (e) {
+  //       if (e.code == 'account-exists-with-different-credential') {
+  //         // handle the error here
+  //       }
+  //       else if (e.code == 'invalid-credential') {
+  //         // handle the error here
+  //       }
+  //     } catch (e) {
+  //       // handle the error here
+  //     }
+  //   }
+
+  //   return user;
+  // }
 }
